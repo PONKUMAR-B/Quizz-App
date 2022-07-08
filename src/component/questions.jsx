903bf4bd318a1eafb1a5ba124cc96d6db040c954
Ponkumar <ponkumarbalasubramanian@gmail.com>
2022-07-08 11:58:06 +0530

    Quiz reactjs

---
+++ src/component/questions.jsx
@@ -0,0 +1,22 @@
+export default[
+    {
+    question: "1 . if ( 10>5 )?",
+    options:["False", "True"],
+    answer: 1,
+    },
+    {
+    question: "2 . a=5,b=10, if (a==b) is the condition true?",
+    options:["Yes", "No"],
+    answer: 1,
+    },
+    {
+    question: "3 . What happened on August 15th in history?",
+    options:["Independence day", "Rupublic day"],
+    answer: 0,
+    },
+    {
+    question: "4 . Are You good developer?",
+    options:["Yes", "No"],
+    answer: 0,
+    }
+]
